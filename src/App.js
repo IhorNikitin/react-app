69fb3ce34f8f902542180ff6f7c53c1db5e44d37
Igor Nikitin <i.nikitin1984@gmail.com>
2018-06-07 01:45:34 +0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import store from './redux';
+import Root from './components/Root';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import history from './history';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello, World!
-      </div>
+	  <Provider store={store}>
+	    <ConnectedRouter history={history} >
+          <Root />
+		</ConnectedRouter>
+	  </Provider>
     );
   }
 }
